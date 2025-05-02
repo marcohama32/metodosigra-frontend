ec698a020f265e4d067300aed4aa14be93249139
Marco Hama <marcohama32@gmail.com>
2025-05-02 08:21:04 +0200

    Corrige rotas e caminhos de imagens para deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import AuthService from '@/services/auth.service';
 
 import Dashboard from "@/views/Dashboard.vue";
@@ -231,7 +231,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   linkActiveClass: "active",
 });
